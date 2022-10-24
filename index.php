5cc84319f39219fbfeaad06ab5d9d3660071517d
Tia Aulia Azahra <tiaaulia701@gmail.com>
2022-10-24 21:40:28 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Tia Aulia Azahra";
+    $umur = 20;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Andi";
+    $umurAdik = "16";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
